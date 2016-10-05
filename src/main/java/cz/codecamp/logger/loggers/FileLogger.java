fd7229390aa17c9c4528e93b71701a3e14b7471f
Jan Hornych <Jan.Hornych@equabank.cz>
2016-10-05 19:41:33 +0200

    FileLogger

---
+++ src/main/java/cz/codecamp/logger/loggers/FileLogger.java
@@ -0,0 +1,30 @@
+package cz.codecamp.logger.loggers;
+
+
+import cz.codecamp.logger.LogLevelEnum;
+import cz.codecamp.logger.LoggerInterface;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class FileLogger implements LoggerInterface {
+
+    private PrintStream stream;
+
+    public FileLogger() {
+        try {
+            stream = new PrintStream(new FileOutputStream(new File("application.log")), true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void log(LogLevelEnum level, String message) {
+        stream.append(level.name() + " " + message);
+        stream.append("\n");
+
+    }
+}
