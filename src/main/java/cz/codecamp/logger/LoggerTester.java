9424cd1ebd169cb4dd83116d2634b2d4d1f610ca
salacovalenka <salacova.lenka@gmail.com>
2016-10-05 20:04:05 +0200

    update

--- src/main/java/cz/codecamp/logger/LoggerTester.java
+++ src/main/java/cz/codecamp/logger/LoggerTester.java
@@ -1,7 +1,13 @@
 package cz.codecamp.logger;
 
+import cz.codecamp.logger.loggers.FileLogger;
+import cz.codecamp.logger.loggers.PrintStreamLogger;
 import cz.codecamp.logger.loggers.StdoutLogger;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,7 +15,7 @@ import java.util.Scanner;
 
 public class LoggerTester {
 
-    private static final Map<String, LogLevelEnum> LEVEL_MAP;
+    private static final Map<String, LogLevelEnum> LEVEL_MAP; // v pameti mi urzuje dvojici klic + hodnota. klic musi byt unikatni (vzdy je prvni)
 
     static {
         Map<String, LogLevelEnum> levelMap = new HashMap<>();
@@ -17,12 +23,21 @@ public class LoggerTester {
         levelMap.put("i", LogLevelEnum.INFO);
         levelMap.put("w", LogLevelEnum.WARNING);
         levelMap.put("e", LogLevelEnum.ERROR);
+        //levelMap.put("d", LogLevelEnum.ERROR); - timhle bych prepsala radek 16
         LEVEL_MAP = Collections.unmodifiableMap(levelMap);
     }
 
     public static void main(String[] args) {
 
-        LoggerInterface logger = new StdoutLogger();
+        //LoggerInterface logger = new StdoutLogger();
+        //LoggerInterface logger = new FileLogger();
+        LoggerInterface logger = new PrintStreamLogger(System.out);
+//        LoggerInterface logger = null;
+//        try {
+//            logger = new PrintStreamLogger(new PrintStream(new FileOutputStream(new File("application.log"), true)));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
 
         for (Scanner scanner = new Scanner(System.in); ; ) {
             System.out.print("> ");
@@ -32,17 +47,17 @@ public class LoggerTester {
             }
 
             String line = scanner.nextLine();
-            String[] parts = line.split("\\s+", 2);
+            String[] parts = line.split("\\s+", 2); //regularni vyraz \\s+ makes one or more times a space, applied just once. vrati retezec na 2 casti, odstranuje oddelovac
 
-            if (parts.length < 2) {
+            if (parts.length < 2) { //jak dlouhy je pole? 0 nebo 1?
                 System.err.println("not enough arguments");
-                continue;
+                continue; // pokud splneno, skacu na radek 28
             }
 
             LogLevelEnum level = LEVEL_MAP.get(parts[0].substring(0, 1).toLowerCase());
             if (level == null) {
                 System.err.println("unknown level [" + parts[0] + "]");
-                continue;
+                continue; // pokud splneno, skacu na radek 28
             }
 
             logger.log(level, parts[1]);
