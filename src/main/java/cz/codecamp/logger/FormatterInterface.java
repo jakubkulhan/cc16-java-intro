b03f73d62ad68ec69104e6e21080215f3818385f
honzapua <honzapua@gmail.com>
2016-10-16 17:23:14 +0200

    Netyka se implementace. Je to JavaDoc doplneni dokumentace. Proto zvlast.

--- src/main/java/cz/codecamp/logger/FormatterInterface.java
+++ src/main/java/cz/codecamp/logger/FormatterInterface.java
@@ -1,5 +1,17 @@
 package cz.codecamp.logger;
 
+/**
+ *Formatuje radku logu. Dela to implementacni trida
+ */
 public interface FormatterInterface {
+    /**
+     * Formatuje radku logu, vklada severitu, vlastni text zpravy, pripadne dalsi informace vychazejici z implementace
+     * tohoto rozhrani
+     *
+     * @param level severity
+     * @param message vlastni zprava logu
+     *
+     * @return naformatovana radka = syrovy retezec primo do file. NEobsahuje EOL.
+     */
     String format(LogLevelEnum level, String message);
 }
